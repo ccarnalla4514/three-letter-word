ae6df7c9e891e66edc6ad31a47b356f57f077372
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-02-18 15:20:54 -0600

    three letter word hackwich

--- three letter word/ViewController.swift
+++ three letter word/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var wordLabel: [UILabel]!
+    
+    @IBOutlet weak var currentLetterLabel: UILabel!
+  
+    @IBOutlet weak var stackView: UIStackView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenTapRecognized(_ sender: UITapGestureRecognizer) {
+    
+    print("Tap was found")
+    
+    }
+    
+    
 }
 
